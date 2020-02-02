dbb5199b895dbdef3d36017cde9614f525ec61dc
cleverjack <example@test.com>
2020-02-02 16:33:36 -0300

    button border-bottom

--- src/components/landing/Catagory.js
+++ src/components/landing/Catagory.js
@@ -6,11 +6,11 @@ import "./css/catagory.css"
      render() {
          return (
             <div className="buttongroup">
-                <Button variant="success">New Sales</Button>
-                <Button variant="info">men</Button>
-                <Button variant="danger">Women</Button>
-                <Button variant="primary">Kids</Button>
-                <Button variant="warning">Others</Button>
+                <Button variant="success" className="newsales">New Sales</Button>
+                <Button variant="info" className="men">men</Button>
+                <Button variant="danger" className="women">Women</Button>
+                <Button variant="primary" className="kids">Kids</Button>
+                <Button variant="warning" className="others">Others</Button>
             </div>
          );
      }
